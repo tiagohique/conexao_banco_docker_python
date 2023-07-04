e00a63b36bce558618ddde0c371477b1d0f117df
Tiago  Silva <tiagosilva@Air-de-Tiago.lan>
2023-07-03 23:45:34 -0300

    Exemplo simples de dokcer e python conectando com Mysql

---
+++ main.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+# Configurações de conexão com o banco de dados
+config = {
+    'user': 'root',
+    'password': '123456',
+    'host': 'localhost'
+}
+
+try:
+    # Estabelece a conexão com o banco de dados
+    connection = mysql.connector.connect(**config)
+    print("Conectado")
+    
+    # Faça qualquer operação desejada com o banco de dados aqui
+    
+    # Encerra a conexão
+    connection.close()
+
+except mysql.connector.Error as error:
+    print("Erro ao conectar ao banco de dados: {}".format(error))
